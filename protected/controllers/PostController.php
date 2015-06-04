9912cc396b1976c1e6e193f19a0d0752dd220821
Olga N <Olga N@Ol4ik>
2015-06-04 16:26:06 +0300

    Add controller Post with action Edit like a class

---
+++ protected/controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PostController
+ *
+ * @author Olga N
+ */
+class PostController extends Controller{
+    //put your code here
+    public function actions() {
+        return array(
+            'edit' => 'application.controllers.post.Edit',
+        );
+    }
+}
---
+++ protected/controllers/post/Edit.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ImladrisAction
+ *
+ * @author Olga N
+ */
+class Edit extends CAction{
+    //put your code here
+    public function run() {
+        echo 11;
+    }
+}
