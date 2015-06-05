9147b064bccc29e5a007a3f3d98d55eff7a81f93
Olga N <Olga N@Ol4ik>
2015-06-05 15:12:26 +0300

    Add params into action Edit of PostController.

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -18,4 +18,9 @@ class PostController extends Controller{
             'edit' => 'application.controllers.post.Edit',
         );
     }
+    public function filters() {
+        return [
+            ['application.filters.SimpleFilter + edit',]
+        ];
+    }
 }
